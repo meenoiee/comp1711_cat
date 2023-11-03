9f3a634c5bc6d67408f925d0396c981dd8ce3e16
meenoiee <146842538+meenoiee@users.noreply.github.com>
2023-11-03 03:31:33 +0000

    savedagain

--- coursework/StepsTask1.c
+++ coursework/StepsTask1.c
@@ -6,7 +6,8 @@
 typedef struct data{
 	char date[11];
 	char time[6];
-	char steps[5];
+	int steps;
+    char charsteps[10];
 } FITNESS_DATA;
 
 // Define any additional variables here
@@ -47,7 +48,7 @@ int main() {
     FILE *file = fopen("FitnessData_2023.csv", "r");
 
     FITNESS_DATA fitness [100000];
-    int linesize = 20;
+    int linesize = 200;
     char inputdata[30];
     int i=0;
     int count=0;
@@ -56,24 +57,26 @@ int main() {
 
     while (fgets(inputdata, linesize, file) != NULL) 
     {
-        tokeniseRecord(inputdata, ",", fitness[i].date, fitness[i].time, fitness[i].steps);
+        tokeniseRecord(inputdata, ",", fitness[i].date, fitness[i].time, fitness[i].charsteps);
 
-        if (strlen(fitness[i].date) > 0 && strlen(fitness[i].time) > 0 && strlen(fitness[i].steps) > 0) 
+        if (strlen(fitness[i].date) > 0 && strlen(fitness[i].time) > 0 && strlen(fitness[i].charsteps) > 0) 
         {
             count++;
+            fitness[i].steps = atoi(fitness[i].charsteps) ;
         }
 
         i++;
     }
+
 
     printf("Number of records in file: %d\n",count);
     
     int validCount = 0;
     for (int j = 0; j < count && validCount < 3; j++) 
     {
-        if (strlen(fitness[j].date) > 0 && strlen(fitness[j].time) > 0 && strlen(fitness[j].steps) > 0) 
+        if (strlen(fitness[j].date) > 0 && strlen(fitness[j].time) > 0 && strlen(fitness[j].charsteps) > 0) 
         {
-            printf("%s/%s/%s\n", fitness[j].date, fitness[j].time, fitness[j].steps);
+            printf("%s/%s/%d\n", fitness[j].date, fitness[j].time, fitness[j].steps);
             validCount++;
         }
     }
